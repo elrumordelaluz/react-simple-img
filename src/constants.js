a0bbf56899b1108a85f0aa4a5dfadc5d53c2191a
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2022-08-05 13:51:01 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const commonStyle = {
 };
 export const defaultImgPlaceholder = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
 export const defaultPlaceholderColor = 'white';
-export const hiddenStyle = { visibility: 'hidden' };
+export const hiddenStyle = { visibility: 'hidden', display: 'none' };
 export const expendWidth = { width: '100%' };
 export const aspectRatioChildStyle = { width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 };
 export const wrapperCommonStyle = {
